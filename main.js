6a0625b9ee29700fc7cf66f5d1be16415cdbbccf
Ruben Garcia <rgarcr09@gmail.com>
2023-04-15 19:24:23 -0400

    Add Custom Size for Grid and Fix Grid Layout
    Add prompt from user for size of grid not greater than 100
    
    Add code to clear previous grid in parent container
    
    Fix grid layout with grid container using flex direction

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 let container = document.querySelector(".grid-container");
-document.querySelector('button').addEventListener('click', makeGrid);
+//Get user input for size of grid that is <= 100 and clear previous grid
+document.querySelector('button').addEventListener('click', (e)=>{
+    if(container.innerHTML.length>1)
+        container.innerHTML='';
+    let size;
+    do{
+        size = Number(prompt('Please enter grid size not greater than 100'));
+    }while(size > 100 && typeof(size) === 'number');
+    makeGrid(size);
+
+});
 /*
 Nested loops to create the rows and cells of the grid
 */
-function makeGrid() {
-    for (let i = 0; i < 16; i++) {
+function makeGrid(size) {
+    for (let i = 0; i < size; i++) {
         const divRow = document.createElement('div');
         divRow.classList.add('row')
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const divCell = document.createElement('div');
             divCell.classList.add('cell')
             divRow.appendChild(divCell)
